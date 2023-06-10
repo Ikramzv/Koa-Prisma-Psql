26e816158b47c8b98dbb3e4b33319ee32e112cd5
Ikram <ikramzulfugar@gmail.com>
2023-06-10 05:05:35 +0400

    controllers added to routes

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,24 +1,40 @@
 import Router from "@koa/router";
+import { User } from "@prisma/client";
+import {
+  createUser,
+  editUsername,
+  getUserByEmailOrName,
+} from "../../controllers/users";
 
 const router = new Router({
-  prefix: "users",
+  prefix: "/users",
   methods: ["POST", "GET", "PATCH"],
 });
 
 // GET USER BY NAME OR EMAIL
 
-router.get("/", (ctx) => {
+router.get("/", async (ctx) => {
   const nameOrEmail = ctx.query.name || ctx.query.email;
+  if (!nameOrEmail)
+    return ctx.throw(400, "Provide email or name to fetch user");
+
+  ctx.body = await getUserByEmailOrName(ctx, nameOrEmail as string);
 });
 
 // EDIT USER NAME
 
-router.patch("/edit_name/:username", (ctx) => {
-  const { username } = ctx.params;
+router.patch("/edit_name/:userId", async (ctx) => {
+  const { userId } = ctx.params;
+  const { username } = ctx.request.body as { username: string };
+  if (!username) return ctx.throw(400, "Provide username");
+  ctx.body = await editUsername(ctx, username, Number(userId));
 });
 
 // CREATE USER
 
-router.post("/create_user", (ctx) => {});
+router.post("/create_user", async (ctx) => {
+  const data = ctx.request.body as User;
+  ctx.body = await createUser(ctx, data);
+});
 
 export default router;

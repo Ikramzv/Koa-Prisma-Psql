3ce9fd39f241c3bc6829a4a7944aee6087f40963
Ikram <ikramzulfugar@gmail.com>
2023-06-10 06:00:52 +0400

    controllers added to route handlers

--- src/routes/posts/index.ts
+++ src/routes/posts/index.ts
@@ -1,28 +1,59 @@
 import Router from "@koa/router";
+import {
+  createPost,
+  deletePost,
+  editPost,
+  getAllPosts,
+  getSinglePost,
+  getUserAllPosts,
+} from "../../controllers/posts";
+import { validate } from "../../middlewares/posts.middleware";
 
 const router = new Router({
   prefix: "/posts",
   methods: ["POST", "GET", "PATCH", "DELETE"],
 });
 
-// GET USER POSTS OR ALL POSTS
+// GET ALL POSTS
 
-router.get("/", (ctx) => {});
+router.get("/", async (ctx) => {
+  ctx.body = await getAllPosts(ctx);
+});
 
-// GET USER POST
+// GET USER'S ALL POSTS
 
-router.get("/:postId", (ctx) => {
+router.get("/user_posts/:userId", async (ctx) => {
+  const { userId } = ctx.params;
+  ctx.body = await getUserAllPosts(ctx, Number(userId));
+});
+
+// GET POST
+
+router.get("/:postId", async (ctx) => {
   const { postId } = ctx.params;
+  ctx.body = await getSinglePost(ctx, Number(postId));
 });
 
 // CREATE POST
 
-router.post("/", (ctx) => {});
+router.post("/", validate, async (ctx) => {
+  const payload = ctx.request.body;
+  ctx.body = await createPost(ctx, payload);
+});
 
 // DELETE POST
 
-router.delete("/:postId", (ctx) => {
+router.delete("/:postId", async (ctx) => {
   const { postId } = ctx.params;
+  ctx.body = await deletePost(ctx, Number(postId));
 });
 
-router.patch("/edit_post", (ctx) => {});
+// EDIT POST
+
+router.patch("/edit_post/:postId", async (ctx) => {
+  const data = ctx.request.body;
+  const { postId } = ctx.params;
+  ctx.body = await editPost(ctx, Number(postId), data);
+});
+
+export default router;

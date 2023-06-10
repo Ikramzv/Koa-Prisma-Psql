2a6f70dba62bfc98603580f26801545208fa9166
Ikram <ikramzulfugar@gmail.com>
2023-06-10 06:00:37 +0400

    added: posts.routes()

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import Router from "@koa/router";
+import postsRouter from "./posts";
 import usersRouter from "./users";
 
 const router = new Router();
 
 router.use(usersRouter.routes()).use(usersRouter.allowedMethods());
+router.use(postsRouter.routes()).use(postsRouter.allowedMethods());
 
 export default router;

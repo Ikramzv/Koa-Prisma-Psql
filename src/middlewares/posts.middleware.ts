f3f1402dffecb490e7c3c383967704fb79bd39fe
Ikram <ikramzulfugar@gmail.com>
2023-06-10 06:00:27 +0400

    fetch user and assign it to ctx.state.user

--- src/middlewares/posts.middleware.ts
+++ src/middlewares/posts.middleware.ts
@@ -1,12 +1,22 @@
 import type { Context, Next } from "koa";
+import { getUserByEmailOrName } from "../controllers/users";
 
-const validate = (ctx: Context, next: Next) => {
+const validate = async (ctx: Context, next: Next) => {
   const { username } = ctx.query;
   if (!username) {
     ctx.status = 400;
     return (ctx.body = "Pass a username as a query parameter");
   }
-  next();
+
+  const user = await getUserByEmailOrName(ctx, username as string);
+
+  if (!user) {
+    ctx.status = 400;
+    return (ctx.body = `There is no user whose username is ${username} in database`);
+  }
+
+  ctx.state.user = user;
+  await next();
 };
 
 export { validate };
